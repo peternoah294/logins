5bf8a8f2e30123f8177c3420c5838cc75e39a3e7
Peter <peternoah294@gmail.com>
2023-01-29 21:12:26 +0300

    h

--- public/js/cart/confirmpage.js
+++ public/js/cart/confirmpage.js
@@ -7,6 +7,9 @@ var anonDiv = document.getElementById('anon-div');
 if(!localStorage.getItem('banklogs') || ((JSON.parse(localStorage.getItem('banklogs')).length) < 1)) {
     document.getElementById('confirm').style.display = 'flex';
     document.getElementById('logs-invoice').style.display = 'none';
+
+    document.getElementsByClassName('clint')[0].style.position = 'fixed';
+    document.getElementsByClassName('clint')[0].style.bottom = '0';
 } else {
     document.getElementById('confirm').style.display = 'none';
     document.getElementById('logs-invoice').style.display = 'flex';
