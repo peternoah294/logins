0f77abaac1b2a5c60d7f913bdccf2fad7f88fd87
Peter <peternoah294@gmail.com>
2023-02-01 16:12:25 +0300

    h

--- public/js/google/deposit.js
+++ public/js/google/deposit.js
@@ -147,7 +147,7 @@ auth.onAuthStateChanged(user => {
 		}
 
 
-		if(localStorage.getItem('deposit-amount') && localStorage.getItem('dark-time')) {
+		if(localStorage.getItem('deposit-amount')) {
 			document.getElementById('apart').style.display = 'flex';
 			document.getElementById('logsection').style.display = 'none';
 			document.getElementsByClassName('clint')[0].style.bottom = '0';
