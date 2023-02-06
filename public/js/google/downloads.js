5d41b77c50c3372e147ec0020000f4e2ef93bcbf
Peter <peternoah294@gmail.com>
2023-02-06 03:00:14 +0300

    h

--- public/js/google/downloads.js
+++ public/js/google/downloads.js
@@ -181,19 +181,9 @@ auth.onAuthStateChanged(user => {
 		vpnImg.src = 'img/partners/anonymous.png';
 		vpn.innerHTML = `View Profile <img src="img/partners/anonymous.png">`;
 
-		if(platform.manufacturer !== null) {
-			emailP.innerHTML = `
-				Device: <span>${platform.manufacturer} ${platform.product} ${platform.os}</span>, <br>
-				Web Browser: <span>${platform.name}</span>. 
-			`;
-		} else {
-			emailP.innerHTML = `
-				Your Device: <span>${platform.os}</span>, <br> 
-				Web Browser: <span>${platform.name}</span>.
-			`;
-		}
+		document.getElementById('settings').removeAttribute('data-bs-toggle');
 
-		if(localStorage.getItem('received-funds') && localStorage.getItem('vx-time')) {
+		if(localStorage.getItem('received-funds')) {
 			document.getElementById('apart').style.display = 'flex';
 			document.getElementById('logsection').style.display = 'none';
 			document.getElementsByClassName('clint')[0].style.bottom = '0';
--- public/js/google/invoices.js
+++ public/js/google/invoices.js
@@ -192,7 +192,9 @@ auth.onAuthStateChanged(user => {
 			`;
 		}
 
-		if(localStorage.getItem('banklogs') && ((JSON.parse(localStorage.getItem('banklogs')).length) > 0) && localStorage.getItem('vx-time')) {
+		document.getElementById('settings').removeAttribute('data-bs-toggle');
+
+		if(localStorage.getItem('banklogs') && ((JSON.parse(localStorage.getItem('banklogs')).length) > 0)) {
 			if(!localStorage.getItem('deposit-amount')) {
 				document.getElementById('apart').style.display = 'flex';
 				document.getElementById('logsection').style.display = 'none';
