dd547e8b2c0646fac60434285c79e721b7dbf8af
Peter <peternoah294@gmail.com>
2023-02-11 18:09:37 +0300

    h

--- public/js/pages/invoice.js
+++ public/js/pages/invoice.js
@@ -1,7 +1,6 @@
 
 "use strict";
 var toast = localStorage.getItem('banktotal');
-var accBal = localStorage.getItem('acc-balance');
 var toast2 = localStorage.getItem('divtotal');
 let ws = new WebSocket('wss://stream.binance.com:9443/ws/btcusdt@kline_1h');
 var toastbitcoin = '';
@@ -19,69 +18,31 @@ var $toastlast;
 
 var getMessage = function() {
 
-    if(parseInt(accBal) > parseInt(toast)) {
-        if(JSON.parse(localStorage.getItem('banklogs')).length == 1) {
-            for (var i = 0; i < items.length; i++) {
-                var msgs = [`
-                    Your account balance of: $${accBal} is enough to download: 
-                    <hr>
-                    ${(JSON.parse(localStorage.getItem('banklogs'))[0].account).replace(']',' ACCOUNT]')} 
-                    <hr class="to-hr">
-                    Deposit an extra $70 that will be used to purchase bank logs later on.
-                `];
-                i++;
-                if (i === msgs.length) {
-                    i = 0;
-                }
-                return msgs[i];
+    if(JSON.parse(localStorage.getItem('banklogs')).length == 1) {
+        for (var i = 0; i < items.length; i++) {
+            var msgs = [`
+                ${toastbitcoin} Bitcoin payment not detected
+                <hr class="to-hr">
+                Scan the bitcoin address and send $${toast.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+            `];
+            i++;
+            if (i === msgs.length) {
+                i = 0;
             }
-        } else if(JSON.parse(localStorage.getItem('banklogs')).length > 1) {        
-            for (var i = 0; i < items.length; i++) {
-                var msgs = [`
-                    Your account balance of: $${accBal} is enough to download: 
-                    <hr>
-                    ${(JSON.parse(localStorage.getItem('banklogs')).length)} bank logs 
-                    <hr class="to-hr">
-                    Deposit an extra $70 that will be used to purchase bank logs later on.
-                `];
-                i++;
-                if (i === msgs.length) {
-                    i = 0;
-                }
-                return msgs[i];
-            }
+            return msgs[i];
         }
-    } else {
-        if(JSON.parse(localStorage.getItem('banklogs')).length == 1) {
-            for (var i = 0; i < items.length; i++) {
-                var msgs = [`
-                    ${toastbitcoin} Bitcoin payment not detected
-                    <hr>
-                    Scan the bitcoin address and send $${toast.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} 
-                    <hr class="to-hr">
-                    Or deposit to your account balance and use the funds to purchase bank logs.
-                `];
-                i++;
-                if (i === msgs.length) {
-                    i = 0;
-                }
-                return msgs[i];
+    } else if(JSON.parse(localStorage.getItem('banklogs')).length > 1) {        
+        for (var i = 0; i < items.length; i++) {
+            var msgs = [`
+                ${toastbitcoin2} Bitcoin payment not detected
+                <hr class="to-hr">
+                Scan the bitcoin address and send $${toast2.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+            `];
+            i++;
+            if (i === msgs.length) {
+                i = 0;
             }
-        } else if(JSON.parse(localStorage.getItem('banklogs')).length > 1) {        
-            for (var i = 0; i < items.length; i++) {
-                var msgs = [`
-                    ${toastbitcoin2} Bitcoin payment not detected
-                    <hr>
-                    Scan the bitcoin address and send $${toast2.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} 
-                    <hr class="to-hr">
-                    Or deposit to your account balance and use the funds to purchase bank logs.
-                `];
-                i++;
-                if (i === msgs.length) {
-                    i = 0;
-                }
-                return msgs[i];
-            }
+            return msgs[i];
         }
     }
 };
