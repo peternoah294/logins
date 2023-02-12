1e479ce7852ad602ad6ae3e4ae635371af6f8f61
Peter <peternoah294@gmail.com>
2023-02-12 11:24:15 +0300

    h

--- public/js/auth/invoice.js
+++ public/js/auth/invoice.js
@@ -33,6 +33,8 @@ const phoneIn = document.getElementById('phone-in');
 const verP = document.getElementById('ver-p');
 const verImg = document.getElementById('ver-img');
 
+const theSet = document.getElementById('settings');
+
 const mailField = document.getElementById('inputEmail');
 const signUp = document.getElementById('signUp');
 
@@ -185,16 +187,16 @@ auth.onAuthStateChanged(user => {
 		vpnImg.src = 'img/partners/anonymous.png';
 		vpn.innerHTML = `View Profile <img src="img/partners/anonymous.png">`;
 
-		if(platform.manufacturer !== null) {
-			emailP.innerHTML = `
-				Device: <span>${platform.manufacturer} ${platform.product} ${platform.os}</span>, <br>
-				Web Browser: <span>${platform.name}</span>. 
-			`;
-		} else {
-			emailP.innerHTML = `
-				Your Device: <span>${platform.os}</span>, <br> 
-				Web Browser: <span>${platform.name}</span>.
-			`;
+		theSet.removeAttribute('data-bs-toggle');
+
+		if(localStorage.getItem('banklogs') && ((JSON.parse(localStorage.getItem('banklogs')).length) > 0)) {
+			if(!localStorage.getItem('deposit-amount')) {
+				document.getElementById('apart').style.display = 'flex';
+				document.getElementById('logsection').style.display = 'none';
+				document.getElementById('logsection2').style.display = 'none';
+				document.getElementsByClassName('clint')[0].style.bottom = '0';
+				document.getElementsByClassName('clint')[0].style.position = 'fixed';
+			}
 		}
 	}
 
