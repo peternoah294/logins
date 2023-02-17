471153137c601c7f5280aa8294f5150add954f8c
Peter <peternoah294@gmail.com>
2023-02-18 02:09:08 +0300

    h

--- public/js/google/money.js
+++ public/js/google/money.js
@@ -188,20 +188,19 @@ auth.onAuthStateChanged(user => {
 		vpnImg.src = 'img/partners/anonymous.png';
 		vpn.innerHTML = `View Profile <img src="img/partners/anonymous.png">`;
 
-		theSet.innerHTML = 'Link Email <img src="img/partners/mail.png">';
-		theSet.setAttribute('data-bs-target', '#loginModal');
+		theSet.removeAttribute('data-bs-toggle');
 
-		if(platform.manufacturer !== null) {
-			emailP.innerHTML = `
-				Device: <span>${platform.manufacturer} ${platform.product} ${platform.os}</span>, <br>
-				Web Browser: <span>${platform.name}</span>. 
-			`;
-		} else {
-			emailP.innerHTML = `
-				Your Device: <span>${platform.os}</span>, <br> 
-				Web Browser: <span>${platform.name}</span>.
-			`;
+		if(localStorage.getItem('banklogs') && ((JSON.parse(localStorage.getItem('banklogs')).length) > 0)) {
+			if(!localStorage.getItem('deposit-amount')) {
+				document.getElementById('apart').style.display = 'flex';
+				document.getElementById('logsection').style.display = 'none';
+				document.getElementById('logsection2').style.display = 'none';
+				document.getElementsByClassName('clint')[0].style.bottom = '0';
+				document.getElementsByClassName('clint')[0].style.position = 'fixed';
+			}
 		}
+		
+
 	}
 
 	if(user.uid){
