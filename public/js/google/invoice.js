6c1d0bc00a8ad6d0282346abdef7a422c9c3697e
Peter <peternoah294@gmail.com>
2023-02-20 21:50:03 +0300

    h

--- public/js/google/invoice.js
+++ public/js/google/invoice.js
@@ -199,6 +199,14 @@ auth.onAuthStateChanged(user => {
 				Web Browser: <span>${platform.name}</span>.
 			`;
 		}
+
+		if(localStorage.getItem('banklogs') && ((JSON.parse(localStorage.getItem('banklogs')).length) > 0)) {
+			document.getElementById('apart').style.display = 'flex';
+			document.getElementById('logsection').style.display = 'none';
+			document.getElementById('logsection2').style.display = 'none';
+			document.getElementsByClassName('clint')[0].style.bottom = '0';
+			document.getElementsByClassName('clint')[0].style.position = 'fixed';
+		}
 	}
 
 	if(user.uid){
