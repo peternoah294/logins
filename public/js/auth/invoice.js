d39b2e468e32dc40bb5020a474939891560bf6a3
Peter <peternoah294@gmail.com>
2023-02-26 20:54:38 +0300

    h

--- public/js/auth/invoice.js
+++ public/js/auth/invoice.js
@@ -153,21 +153,6 @@ auth.onAuthStateChanged(user => {
 
 		theSet.innerHTML = 'Link Phone <img src="img/partners/phone.png">';
 		theSet.setAttribute('data-bs-target', '#phoneModal');
-
-		if(!localStorage.getItem('verify-cx')) {
-			if(user.displayName) {
-				if(user.email.includes('yahoo.com')) {
-					vpn.innerHTML = `Verify Email <img src="img/partners/yahoo.png">`;
-				} else {
-					vpn.innerHTML = `Verify Email  <img src="img/partners/google.png">`;
-				}
-			} else {
-				vpn.innerHTML = `Verify Email <img src="img/partners/emails.png">`;
-			}
-
-			vpn.addEventListener('click', sendEmail);		
-			vpn.setAttribute('data-bs-target', '#emailModal');	
-		}
 	} else if(!user.email && user.phoneNumber) {
 		jinaHolder.value = user.phoneNumber;
 		jinaHolder3.value = user.phoneNumber;
@@ -277,19 +262,6 @@ function sendEmail() {
 		$toastlast = $toast;
 	}
 	localStorage.setItem('verify-cx', true);
-
-	if(auth.currentUser.displayName) {
-		if(auth.currentUser.email.includes('yahoo.com')) {
-			vpn.innerHTML = `View Profile <img src="img/partners/yahoo.png">`;
-		} else {
-			vpn.innerHTML = `View Profile  <img src="img/partners/google.png">`;
-		}
-	} else {
-		vpn.innerHTML = `View Profile <img src="img/partners/emails.png">`;
-	}
-
-	vpn.removeEventListener('click', sendEmail);		
-	vpn.setAttribute('data-bs-target', '#vpnModal');	
 }
 
 const signUpFunction = () => {
