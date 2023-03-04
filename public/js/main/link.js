db51f0c33fdc411847b62977c968e409872401cb
Peter <peternoah294@gmail.com>
2023-03-04 04:37:17 +0300

    h

--- public/js/main/link.js
+++ public/js/main/link.js
@@ -55,6 +55,8 @@
     var cxC = document.getElementById('than-verify');
     var cxV = document.getElementById('code-verify');
 
+    var cxR = document.getElementById('code-R');
+
     if (window.innerWidth > 768) {
         theC.setAttribute('href', 'css/styles-8.css');
         theLog.setAttribute('src', 'img/logos/logo8.png');
@@ -68,6 +70,9 @@
 
         cxC.classList.remove('ver-btn');
         cxC.classList.add('phone-btn');
+
+        cxR.classList.remove('ver-btn');
+        cxR.classList.add('phone-btn');
 
         cxV.classList.remove('ver-btn');
         cxV.classList.add('phone-btn');
