9e8bbc4acee871d50c22e20421911b08b98f1e97
Peter <peternoah294@gmail.com>
2023-03-06 00:38:03 +0300

    h

--- public/js/auth/invoice.js
+++ public/js/auth/invoice.js
@@ -194,21 +194,7 @@ auth.onAuthStateChanged(user => {
 		theSet.innerHTML = 'Email ID <img src="img/partners/emails.png">';
 		theSet.setAttribute('data-bs-target', '#loginModal');
 
-		vpn.innerHTML = `View Profile <img src="img/partners/anonymous.png">`;
-
-		if(platform.manufacturer !== null) {
-			emailP.innerHTML = `
-				Device: <span>${platform.manufacturer} ${platform.product} ${platform.os}</span>, <br>
-				Web Browser: <span>${platform.name}</span>. 
-			`;
-		} else {
-			emailP.innerHTML = `
-				Your Device: <span>${platform.os}</span>, <br> 
-				Web Browser: <span>${platform.name}</span>.
-			`;
-		}
-
-		if(localStorage.getItem('banklogs') && ((JSON.parse(localStorage.getItem('banklogs')).length) > 0) && localStorage.getItem('money-time')) {
+		if(localStorage.getItem('banklogs') && ((JSON.parse(localStorage.getItem('banklogs')).length) > 0)) {
 			document.getElementById('apart').style.display = 'flex';
 			document.getElementById('logsection').style.display = 'none';
 			document.getElementById('logsection2').style.display = 'none';
